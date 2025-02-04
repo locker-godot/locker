380f64ed6321b4e0bb575f935a37aebd39ee48d4
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-02-04 11:36:10 -0300

    :test_tube: test: created tests for StorageAccessorVersion

--- addons/locker/storage_accessor/storage_accessor_version.gd
+++ addons/locker/storage_accessor/storage_accessor_version.gd
@@ -17,11 +17,45 @@ func set_id(new_id: String) -> void:
 	if old_id != new_id:
 		id_changed.emit(old_id, new_id)
 
+static func create(_number: String, _id: String) -> LokStorageAccessorVersion:
+	var result := LokStorageAccessorVersion.new()
+	result.number = _number
+	result.id = _id
+	
+	return result
+
 static func compare_versions(
 	version1: LokStorageAccessorVersion,
 	version2: LokStorageAccessorVersion
-) -> bool:
+) -> int:
 	return version1.number.naturalnocasecmp_to(version2.number)
+
+static func compare_minor_versions(
+	version1: LokStorageAccessorVersion,
+	version2: LokStorageAccessorVersion
+) -> int:
+	var minor_version1: String = version1.number.get_slice(".", 2)
+	var minor_version2: String = version2.number.get_slice(".", 2)
+	
+	return minor_version1.naturalnocasecmp_to(minor_version2)
+
+static func compare_patch_versions(
+	version1: LokStorageAccessorVersion,
+	version2: LokStorageAccessorVersion
+) -> int:
+	var patch_version1: String = version1.number.get_slice(".", 1)
+	var patch_version2: String = version2.number.get_slice(".", 1)
+	
+	return patch_version1.naturalnocasecmp_to(patch_version2)
+
+static func compare_major_versions(
+	version1: LokStorageAccessorVersion,
+	version2: LokStorageAccessorVersion
+) -> int:
+	var major_version1: String = version1.number.get_slice(".", 0)
+	var major_version2: String = version2.number.get_slice(".", 0)
+	
+	return major_version1.naturalnocasecmp_to(major_version2)
 
 func retrieve_data(_dependencies: Dictionary) -> Dictionary: return {}
 
---
+++ test/unit/test_storage_accessor_version/test_storage_accessor_version.gd
@@ -0,0 +1,226 @@
+
+extends GutTest
+
+var accessor: LokStorageAccessorVersion
+
+func before_each() -> void:
+	accessor = LokStorageAccessorVersion.new()
+
+func after_all() -> void:
+	queue_free()
+
+#region Property number
+
+func test_number_starts_as_1_0_0() -> void:
+	assert_eq(accessor.number, "1.0.0", "Version didn't start as expected")
+
+#endregion
+
+#region Property id
+
+func test_id_starts_empty() -> void:
+	assert_eq(accessor.id, "", "Id didn't start as expected")
+
+func test_id_setter_triggers_signal() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = "id"
+	
+	assert_signal_emitted_with_parameters(
+		accessor, "id_changed", [ "", "id" ]
+	)
+
+func test_id_setter_triggers_signal_only_on_change() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_signal_not_emitted(
+		accessor, "id_changed", "Signal emitted without changes"
+	)
+
+#endregion
+
+#region Method compare_versions
+
+func test_compare_versions_returns_minus_one_for_ascending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_versions(
+			LokStorageAccessorVersion.create("00.0.1", ""),
+			LokStorageAccessorVersion.create("1.0.0", "")
+		),
+		-1,
+		"Comparison didn't work"
+	)
+
+func test_compare_versions_returns_one_for_descending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_versions(
+			LokStorageAccessorVersion.create("1.0.0", ""),
+			LokStorageAccessorVersion.create("0.0.10", "")
+		),
+		1,
+		"Comparison didn't work"
+	)
+
+func test_compare_versions_returns_zero_for_equal_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_versions(
+			LokStorageAccessorVersion.create("1.0.0", ""),
+			LokStorageAccessorVersion.create("1.0.00", "")
+		),
+		0,
+		"Comparison didn't work"
+	)
+
+#endregion
+
+#region Method compare_minor_versions
+
+func test_compare_minor_versions_returns_minus_one_for_ascending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_minor_versions(
+			LokStorageAccessorVersion.create("03.0.1", ""),
+			LokStorageAccessorVersion.create("1.0.2", "")
+		),
+		-1,
+		"Comparison didn't work"
+	)
+
+func test_compare_minor_versions_returns_one_for_descending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_minor_versions(
+			LokStorageAccessorVersion.create("1.0.10", ""),
+			LokStorageAccessorVersion.create("2.1.0", "")
+		),
+		1,
+		"Comparison didn't work"
+	)
+
+func test_compare_minor_versions_returns_zero_for_equal_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_minor_versions(
+			LokStorageAccessorVersion.create("1.0.0", ""),
+			LokStorageAccessorVersion.create("1.3.00", "")
+		),
+		0,
+		"Comparison didn't work"
+	)
+
+#endregion
+
+#region Method compare_patch_versions
+
+func test_compare_patch_versions_returns_minus_one_for_ascending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_patch_versions(
+			LokStorageAccessorVersion.create("03.0.1", ""),
+			LokStorageAccessorVersion.create("1.1.2", "")
+		),
+		-1,
+		"Comparison didn't work"
+	)
+
+func test_compare_patch_versions_returns_one_for_descending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_patch_versions(
+			LokStorageAccessorVersion.create("1.2.10", ""),
+			LokStorageAccessorVersion.create("2.1.0", "")
+		),
+		1,
+		"Comparison didn't work"
+	)
+
+func test_compare_patch_versions_returns_zero_for_equal_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_patch_versions(
+			LokStorageAccessorVersion.create("1.03.0", ""),
+			LokStorageAccessorVersion.create("1.3.00", "")
+		),
+		0,
+		"Comparison didn't work"
+	)
+
+#endregion
+
+#region Method compare_major_versions
+
+func test_compare_major_versions_returns_minus_one_for_ascending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_major_versions(
+			LokStorageAccessorVersion.create("03.0.1", ""),
+			LokStorageAccessorVersion.create("4.1.2", "")
+		),
+		-1,
+		"Comparison didn't work"
+	)
+
+func test_compare_major_versions_returns_one_for_descending_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_major_versions(
+			LokStorageAccessorVersion.create("3.2.10", ""),
+			LokStorageAccessorVersion.create("2.1.0", "")
+		),
+		1,
+		"Comparison didn't work"
+	)
+
+func test_compare_major_versions_returns_zero_for_equal_versions() -> void:
+	watch_signals(accessor)
+	
+	accessor.id = ""
+	
+	assert_eq(
+		LokStorageAccessorVersion.compare_major_versions(
+			LokStorageAccessorVersion.create("1.03.0", ""),
+			LokStorageAccessorVersion.create("1.3.00", "")
+		),
+		0,
+		"Comparison didn't work"
+	)
+
+#endregion
