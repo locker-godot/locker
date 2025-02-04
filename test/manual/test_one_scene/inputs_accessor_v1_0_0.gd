f79cc5bbae0f790cefd5f0bda19e4d52929dce2c
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-02-04 11:03:20 -0300

    :test_tube: refac: conforming tests to new architecture

---
+++ test/manual/test_one_scene/inputs_accessor_v1_0_0.gd
@@ -0,0 +1,30 @@
+@tool
+class_name InputsAccessorV1_0_0
+extends LokStorageAccessorVersion
+
+func retrieve_data(dependencies: Dictionary) -> Dictionary:
+	var color: ColorPickerButton = dependencies.get("color")
+	var name_input: ColorPickerButton = dependencies.get("name_input")
+	
+	var result := {}
+	
+	if color != null:
+		result["color"] = var_to_str(color.color)
+	if name_input != null:
+		result["name"] = name_input.text
+	
+	return result
+
+func consume_data(
+	data: Dictionary,
+	dependencies: Dictionary
+) -> void:
+	var color: ColorPickerButton = dependencies.get("color")
+	var name_input: ColorPickerButton = dependencies.get("name_input")
+	
+	if color != null and data.has("color"):
+		color.color = str_to_var(data["color"])
+		
+		color.color_changed.emit(color.color)
+	if name_input != null and data.has("name"):
+		name_input.text = data["name"]
