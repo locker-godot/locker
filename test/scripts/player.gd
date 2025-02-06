8f2bb43c27ceace99b045951b72c18b8775862d4
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-02-06 11:18:07 -0300

    :test_tube: refac: changing manual test architecture

--- test/scripts/player.gd
+++ test/scripts/player.gd
@@ -1,9 +1,16 @@
+
 extends Node2D
 
-func _ready() -> void:
-	pass
+const SPEED: float = 256.0
+
+@export var inputs: Dictionary = {
+	"up": "player1_up",
+	"right": "player1_right",
+	"down": "player1_down",
+	"left": "player1_left"
+}
 
 func _process(delta: float) -> void:
 	global_position += Input.get_vector(
-		"left", "right", "up", "down"
-	) * 256.0 * delta
+		inputs["left"], inputs["right"], inputs["up"], inputs["down"]
+	) * SPEED * delta
