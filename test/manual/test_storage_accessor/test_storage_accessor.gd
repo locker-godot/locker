03bbda88d1e35a8e56acbb348568aef31925977a
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-02-08 10:25:29 -0300

    :test_tube: test: created more code in the manual Accessor test

--- test/manual/test_storage_accessor/test_storage_accessor.gd
+++ test/manual/test_storage_accessor/test_storage_accessor.gd
@@ -2,6 +2,33 @@ extends Node
 
 @onready var accessor: LokStorageAccessor = $Accessor
 
+class VersionTest extends LokStorageAccessorVersion:
+	
+	var data: Dictionary = {}
+	
+	func retrieve_data(_dep: Dictionary) -> Dictionary:
+		return {
+			"a": 1,
+			"b": 2,
+			"c": 3
+		}
+	
+	func consume_data(_data: Dictionary, _dep: Dictionary) -> void:
+		data = _data
+	
+
+func version_number_setter() -> void:
+	#accessor.versions = [
+		#LokStorageAccessorVersion.create("2.1.2"),
+		#LokStorageAccessorVersion.create("1.0.1")
+	#]
+	#accessor.versions.append(LokStorageAccessorVersion.create("2.1.2"))
+	#accessor.versions.append(LokStorageAccessorVersion.create("1.0.0"))
+	#accessor.update_version()
+	
+	print(accessor.version_number)
+	print(accessor.version)
+
 func find_latest_version() -> void:
 	var result: String
 	
@@ -15,5 +42,31 @@ func find_latest_version() -> void:
 	
 	print(result)
 
+func retrieve_data() -> void:
+	var version: LokStorageAccessorVersion = VersionTest.new()
+	
+	accessor.versions = [ version ]
+	accessor.active = false
+	
+	var result: Dictionary = accessor.retrieve_data()
+	
+	print(result)
+
+func consume_data() -> void:
+	var version: LokStorageAccessorVersion = VersionTest.new()
+	
+	accessor.versions = [ version ]
+	accessor.active = false
+	
+	accessor.consume_data({
+		"a": 1,
+		"b": 2,
+		"c": 3
+	})
+	
+	var result: Dictionary = accessor.version.data
+	
+	print(result)
+
 func _ready() -> void:
-	find_latest_version()
+	consume_data()
