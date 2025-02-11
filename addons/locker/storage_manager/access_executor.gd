56e68d5563fcbfe96e0e88f50965579c1f3d1a8a
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-02-10 21:55:37 -0300

    :recycle: refac: changed AccessExecutor to use Dict instead of Enum

--- addons/locker/storage_manager/access_executor.gd
+++ addons/locker/storage_manager/access_executor.gd
@@ -2,36 +2,60 @@
 class_name LokAccessExecutor
 extends Resource
 
-enum Operation {
-	NONE,
-	SAVE,
-	LOAD,
-	REMOVE
-}
+signal operation_finished(result: Dictionary, operation: StringName)
 
-signal operation_finished(result: Dictionary, operation: Operation)
 signal saving_finished(result: Dictionary)
+
 signal loading_finished(result: Dictionary)
+
 signal reading_finished(result: Dictionary)
+
 signal removing_finished(result: Dictionary)
 
+var operations: Dictionary = {
+	&"save": {
+		&"callable": save_data,
+		&"signal": saving_finished
+	},
+	&"load": {
+		&"callable": load_data,
+		&"signal": loading_finished
+	},
+	&"read": {
+		&"callable": read_data,
+		&"signal": reading_finished
+	},
+	&"remove": {
+		&"callable": remove_data,
+		&"signal": removing_finished
+	}
+}
+
 var mutex: Mutex = Mutex.new()
+
 var semaphore: Semaphore = Semaphore.new()
+
 var thread: Thread = Thread.new()
+
 var exit_executor: bool = false
 
-var current_operation: Operation = Operation.NONE
+var current_operation: String = &""
+
+var access_strategy: LokAccessStrategy = null
 
 func _init() -> void:
-	thread.start(execute)
+	start_execution()
 
-func push_error_executor_busy(tried_operation: Operation) -> void:
-	push_error("Can't start %s, executor is busy with %s" % [
-		tried_operation
-	])
+func push_error_executor_busy(tried_operation: StringName) -> void:
+	push_error(
+		"Can't start '%s' operation, executor is busy with '%s' operation" % [
+			tried_operation,
+			current_operation
+		]
+	)
 
 func start_execution() -> void:
-	pass
+	thread.start(execute)
 
 func execute() -> void:
 	while true:
@@ -44,12 +68,14 @@ func execute() -> void:
 		if should_stop:
 			break
 		
-		var operation_callable: Callable = get_operation_callable(current_operation)
+		var operation_callable: Callable = get_operation_callable(
+			current_operation
+		)
 		
 		operation_callable.call()
 		
 		mutex.lock()
-		current_operation = Operation.NONE
+		current_operation = &""
 		mutex.unlock()
 
 func finish_execution() -> void:
@@ -61,36 +87,81 @@ func finish_execution() -> void:
 	
 	thread.wait_to_finish()
 
-func get_operation_callable(operation: Operation) -> Callable:
-	match operation:
-		Operation.SAVE: return save_data
-		Operation.LOAD: return load_data
-		Operation.REMOVE: return remove_data
-		_: return Callable()
+func get_operation_data(operation: String) -> Dictionary:
+	return operations.get(operation, {})
 
-func get_operation_string(operation: Operation) -> String:
-	match operation:
-		Operation.SAVE: return "SAVE"
-		Operation.LOAD: return "LOAD"
-		Operation.REMOVE: return "REMOVE"
-		_: return "INVALID"
+func set_operation_callable(operation: String, callable: Callable) -> void:
+	get_operation_data(operation)["callable"] = callable
+
+func get_operation_callable(operation: String) -> Callable:
+	var operation_data: Dictionary = get_operation_data(operation)
+	
+	return operation_data.get("callable", Callable())
+
+func get_operation_signal(operation: String) -> Signal:
+	var operation_data: Dictionary = get_operation_data(operation)
+	
+	return operation_data.get("signal", Signal())
+
+func create_result(
+	data: Dictionary = {},
+	status: Error = Error.OK
+) -> Dictionary:
+	return {
+		"status": status,
+		"data": data
+	}
+
+func reset_current_operation() -> void:
+	current_operation = &""
 
 func is_busy() -> bool:
-	return current_operation != Operation.NONE
+	return current_operation != &""
 
-func start_saving() -> void:
-	operate(Operation.SAVE)
-	
+func is_saving() -> bool:
+	return current_operation == &"save"
+
+func is_loading() -> bool:
+	return current_operation == &"load"
+
+func is_reading() -> bool:
+	return current_operation == &"read"
+
+func is_removing() -> bool:
+	return current_operation == &"remove"
+
+func start_saving(
+	file_path: String,
+	file_format: String,
+	data: Dictionary,
+	replace: bool = false
+) -> void:
+	operate(&"save", [ file_path, file_format, data, replace ])
+
 func start_loading() -> void:
-	operate(Operation.LOAD)
+	#operate(Operation.LOAD)
+	pass
+
+func start_reading() -> void:
+	#operate(Operation.READ)
+	pass
 	
 func start_removing() -> void:
-	operate(Operation.REMOVE)
+	#operate(Operation.REMOVE)
+	pass
 
-func operate(operation: Operation) -> bool:
+func finish_operation(result: Dictionary, operation_name: StringName) -> void:
+	operation_finished.emit(result, operation_name)
+	get_operation_signal(operation_name).emit(result)
+
+func operate(operation: StringName, args: Array) -> bool:
 	if is_busy():
 		push_error_executor_busy(operation)
 		return false
+	
+	set_operation_callable(
+		operation, get_operation_callable(operation).bindv(args)
+	)
 	
 	mutex.lock()
 	current_operation = operation
@@ -100,18 +171,67 @@ func operate(operation: Operation) -> bool:
 	
 	return true
 
+func save_data(
+	file_path: String,
+	file_format: String,
+	data: Dictionary,
+	replace: bool = false
+) -> Dictionary:
+	var result: Dictionary = create_result()
+	
+	if access_strategy == null:
+		result["status"] = Error.ERR_UNCONFIGURED
+		
+		finish_operation(result, &"save")
+		return result
+	
+	print("%s: Started saving file %s;" % [
+		Time.get_ticks_msec(),
+		file_path
+	])
+	
+	result["data"] = access_strategy.save_data(
+		file_path, file_format, data, replace
+	)
+	
+	finish_operation(result, &"save")
+	
+	print("%s: Finished saving file %s;" % [
+		Time.get_ticks_msec(),
+		file_path
+	])
+	
+	return result
+
 # Blocking operation
-func save_data() -> Dictionary:
-	return {}
+#func save_data() -> Dictionary:
+	#for i: int in 25_000_000:
+		#i += 1
+	#
+	#var result: Dictionary = { "saved": true }
+	#
+	#operation_finished.emit(result, Operation.SAVE)
+	#saving_finished.emit(result)
+	#
+	#return result
 
 # Blocking operation
 func load_data() -> Dictionary:
-	return {}
+	for i: int in 10_000_000:
+		i += 1
+	
+	return { "loaded": true }
 
 # Blocking operation
 func read_data() -> Dictionary:
-	return {}
+	for i: int in 10_000_000:
+		i += 1
+	
+	return { "readed": true }
 
 # Blocking operation
 func remove_data() -> Dictionary:
-	return {}
+	for i: int in 10_000_000:
+		i += 1
+	
+	return { "removed": true }
