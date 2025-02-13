f7c1eb7fdfb17d196a0101a43669d4538648b9ce
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-02-13 14:51:38 -0300

    :sparkles: code: added more utility methods

--- addons/locker/util/util.gd
+++ addons/locker/util/util.gd
@@ -9,3 +9,38 @@ static func filter_value(filter: Array, value: Variant) -> bool:
 		return true
 	
 	return false
+
+static func filter_dictionary(dict: Dictionary, filter: Callable) -> Dictionary:
+	var result: Dictionary = {}
+	
+	for key: Variant in dict.keys():
+		var value: Variant = dict[key]
+		
+		if filter.call(key, value):
+			result[key] = value
+	
+	return result
+
+static func split_dictionary(dict: Dictionary, spliter: Callable) -> Array[Dictionary]:
+	var truthy_dict: Dictionary = {}
+	var falsy_dict: Dictionary = {}
+	
+	for key: Variant in dict.keys():
+		var value: Variant = dict[key]
+		
+		if spliter.call(key, value):
+			truthy_dict[key] = value
+		else:
+			falsy_dict[key] = value
+	
+	return [ truthy_dict, falsy_dict ]
+
+static func map_dictionary(dict: Dictionary, mapper: Callable) -> Dictionary:
+	var result: Dictionary = {}
+	
+	for key: Variant in dict.keys():
+		var value: Variant = dict[key]
+		
+		result[key] = mapper.call(key, value)
+	
+	return result
